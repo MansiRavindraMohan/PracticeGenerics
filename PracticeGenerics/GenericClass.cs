9793e10a323e4c5f0acf2e04ba1ab6cc5468248d
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-09 09:08:45 +0530

    Add: Program for basics of generics

---
+++ PracticeGenerics/GenericClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeGenerics
+{
+    public class GenericClass<T>
+    {
+        //Variable of type T
+        public T data;
+
+        //This is my generic constructor
+        public GenericClass(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data : " + this.data);
+        }
+        public void GenericMethod(T data)
+        {
+            Console.WriteLine("Data : " + data);
+        }
+        public void GenericMethodOne<T>(T data)
+        {
+            Console.WriteLine("Data : " + data);
+        }
+    }
+}
---
+++ PracticeGenerics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeGenerics
+{
+    class Program
+    {
+        //Generic means the general form.
+        //It does not contain any data type.
+        //Generic means not a specific to a particular data type.
+        //<>This is angle bracket with type name.
+        //TypeName<T> T is type parameter.
+        //Generic allow us to create a single class or method that can be used with different data type.
+        //Advantages-We can reuse the code.
+        public static void Main(string[] args)
+        {
+            GenericClass<string> genericClass= new GenericClass<string>("Mansi");
+            GenericClass<int> generic = new GenericClass<int>(45);
+            GenericClass<double> dGeneric = new GenericClass<double>(3.6);
+            
+            genericClass.GenericMethod("Hello");
+            generic.GenericMethod(23);
+            dGeneric.GenericMethod(1.4);
+
+            genericClass.GenericMethodOne<string>("Hii");
+            generic.GenericMethodOne<int>(48);
+            dGeneric.GenericMethodOne<double>(2.5);
+            Console.ReadLine();
+        }
+    }
+}
